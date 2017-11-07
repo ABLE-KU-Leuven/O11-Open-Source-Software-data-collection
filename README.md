32ac28e6f84592e24175ec5719b9f9a329cb34e6
Unknown <martijn.millecamp@hotmail.com>
2017-11-07 16:07:00 +0100

    Cleaning up + all grades in 1 file

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 UploadData
+
+Little piece of code to upload grades of students
+
+Users can select a faculty, a program and a course from a dropdown.
+  These options are shown in options.json
+  The update of the dropdown happens in showDropdown.js
+We expect the user to upload a csv file (, or ; separated) with two columns:
+  studentid and grades
+  The studentid must be an integer
+  The grades must be an integer between 0 and 20 or # or NA
+  This checking happens in upload.php
+We save the grades in ../uploads
+
+When the grades are uploaded, we convert all of them in a json file
+  We do this in the parseCSV.py file
+  run python parseCSV.Py
+  
--- parseCSV.py
+++ parseCSV.py
@@ -3,6 +3,11 @@ import csv
 import json
 
 def getFaculty(filename):
+    """
+    Find faculty in given filename
+    filename is built as directory/facultyprogramcourse.csv
+    All programs starts with ABA or SMA
+    """
     ABA =  filename.split('ABA')
     SMA =  filename.split('SMA')
     if len(ABA) > 1:
@@ -44,30 +49,75 @@ def getRemaining(filename, string):
     return filename.replace(string, '')
 
 def getSemester(course):
+    """
+    Get the semester based on the name of the course
+    """
     if course.startswith('TTT'):
         return -1
     else:
         return -2
 
 def getCourse(courseextension):
+    """
+    Given course.extension
+    return course
+    """
     return courseextension.split('.')[0]
 
-def writeGradesCSV(filename, writepath, course):
+def writeGradesCSV(filepath, filename, writepath):
+    """
+    filename: path of input file (only studentid and grade)
+    writepath: path of output file of complete csv file (multiple fields)
+    """
+    # get all field values based on filename
+    faculty = getFaculty(filename)
+    programcourse = getRemaining(filename, faculty)
+    program = getProgram(programcourse)
+    courseextension = getRemaining(programcourse, program)
+    course = getCourse(courseextension)
+
     gradesToCSV = []
-    with open(filename, 'rb') as csvfile:
+    with open(filepath, 'rb') as csvfile:
         next(csvfile)
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
              studentid = row[0]
              score = row[1]
-             gradesToCSV.append([studentid, course, score, 'J', '2016-2017', score, '#'])
+             gradesToCSV.append([faculty, program, studentid, course, score, 'J', '2016-2017', score, '#'])
+    # If the file not exist, write the fields to it
     if not os.path.isfile(writepath):
-        gradesToCSV.insert(0,['studentid','courseid', 'finalscore', 'generatiestudent', 'year', 'grade_try1', 'grade_try2'])
+        gradesToCSV.insert(0,['faculty', 'program','studentid','courseid', 'finalscore', 'generatiestudent', 'year', 'grade_try1', 'grade_try2'])
 
     with open(writepath, 'a') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         for row in gradesToCSV:
             writer.writerow(row)
+
+def writeCoursesCSV(filepath, filename, writepath):
+    """
+    filename: path of input file (only studentid and grade)
+    writepath: path of output file of complete csv file (multiple fields)
+    """
+    # get all field values based on filename
+    faculty = getFaculty(filename)
+    programcourse = getRemaining(filename, faculty)
+    program = getProgram(programcourse)
+    courseextension = getRemaining(programcourse, program)
+    course = getCourse(courseextension)
+    semester = getSemester(course)
+
+    coursesToCSV = []
+
+    coursesToCSV.append([faculty, program, course, 0, course, semester, 1, '#' ])
+    # If the file not exist, write the fields to it
+    if not os.path.isfile(writepath):
+        coursesToCSV.insert(0,['faculty', 'program','courseid', 'credits', 'coursename', 'semester', 'phase', 'block'])
+
+    with open(writepath, 'a') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        for row in coursesToCSV:
+            writer.writerow(row)
+
 
 def writeToJSON(csvfile):
     filename, file_extension = os.path.splitext(csvfile)
@@ -75,34 +125,51 @@ def writeToJSON(csvfile):
     jsonfile = open('JSON/' + filename + ".json", 'w')
 
     fieldnames = csvfile.readline().rstrip('\r\n').split(',')
-    print fieldnames
     reader = csv.DictReader( csvfile, fieldnames)
     for row in reader:
+        row = castRow(row)
         json.dump(row, jsonfile)
         jsonfile.write('\n')
 
-directory = '../upload/'
-coursesToCSV = [['faculty', 'program','courseid', 'credits', 'coursename', 'semester', 'phase', 'block']]
-files = []
-for filename in os.listdir(directory):
-    if filename.endswith(".csv"):
-        files.append(filename)
-        faculty = getFaculty(filename)
-        programcourse = getRemaining(filename, faculty)
-        program = getProgram(programcourse)
-        courseextension = getRemaining(programcourse, program)
-        course = getCourse(courseextension)
-        semester = getSemester(course)
-        coursesToCSV.append([faculty, program, course, 0, course, semester, 1, '#' ])
-        filepath = directory + filename
-        writepath = 'output/'+ faculty + '_'+ program + '_' + "grades.csv"
-        writeGradesCSV(filepath, writepath, course)
-    else:
-        continue
-with open('output/courses.csv', 'wb') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',')
-    for row in coursesToCSV:
-        writer.writerow(row)
+def castRow(row):
+    """
+    Cast the values in inkeys to integers
+    """
+    newRow = {}
+    intkeys = ['studentid', 'finalscore', 'grade_try1', 'grade_try2', 'credits', 'semester']
+    for key,value in row.iteritems():
+        if key in intkeys:
+            try:
+                newRow[key] = int(value)
+            except:
+                newRow[key] = value
+                pass
+        else:
+            newRow[key] = value
+    return newRow
 
-for filename in os.listdir('output/'):
-    writeToJSON(filename)
+def main(directory):
+    """
+    Parse all (csv)files located in directory to json
+    First we make csv files of it and store it in /output
+    Then we convert csv files to json and store it in /JSON
+    """
+
+    # array where we save
+    files = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".csv"):
+            files.append(filename)
+            filepath = directory + filename
+            coursespath = 'output/courses.csv'
+            gradespath = 'output/grades.csv'
+            writeGradesCSV(filepath, filename,  gradespath)
+            writeCoursesCSV(filepath, filename, coursespath)
+        else:
+            continue
+
+    for filename in os.listdir('output/'):
+        writeToJSON(filename)
+
+directory = '../upload/'
+main(directory)
